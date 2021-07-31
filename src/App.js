d7f4d5ddccbfb1b3abd794ed720af8c5cb5ea8fb
unknown <aanchalannu02@gmail.com>
2021-07-31 14:37:51 +0530

    updations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import RecipeTile from './RecipeTile.js';
 import Axios from "axios";
 import { useState } from "react";
 
@@ -7,11 +8,12 @@ function App() {
 
   const [query, setquery] = useState("");
   const [recipes, setrecipes] = useState([]);
+  const [healthLabels, sethealthLabels] = useState("vegan");
 
   const YOUR_APP_ID = "f09372fb";
 const YOUR_APP_KEY ="a5bb860002f44a9a4be78bf5bb2183a7	";
 
-  var url =`https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&&health=alcohol-free`;
+  var url =`https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&health=${healthLabels}`;
 
   async function getRecipes() {
     var result= await Axios.get(url);
@@ -30,9 +32,22 @@ const YOUR_APP_KEY ="a5bb860002f44a9a4be78bf5bb2183a7	";
       <form className="app__searchForm" onSubmit={onSubmit}>
         <input type="text" className= "app__input" placeholder="Enter Ingridient" value={query} onChange={(e) => setquery(e.target.value)}/>
         <input className="app__submit" type ="submit" value = "Search" />
-
+        <select className="app__healthLabels">
+        <option onClick= {() => sethealthLabels("vegan")}>Vegan</option>
+        <option onClick= {() => sethealthLabels("alcohol_free")}>Alcohol-free</option>
+        <option onClick= {() => sethealthLabels("immune_supportive")}>Immune-Supportive</option>
+        <option onClick= {() => sethealthLabels("celery_free")}>Celery-free</option>
+        <option onClick= {() => sethealthLabels("dairy")}>Dairy</option>
+        <option onClick= {() => sethealthLabels("eggs")}>Eggs</option>
+        <option onClick= {() => sethealthLabels("fish")}>Fish</option>
+        <option onClick= {() => sethealthLabels("keto")}>Keto</option>
+        <option onClick= {() => sethealthLabels("sugar_conscious")}>Sugar-conscious</option>
+        <option onClick= {() => sethealthLabels("low_potassium")}>Low potassium</option>
+        <option onClick= {() => sethealthLabels("no_sugar")}>No-suga</option>
+        </select>
+      
       </form>
-      <div>
+      <div className="app__recipes">
         {recipes.map((recipe) => {
           return <RecipeTile recipe={recipe}/>;
         })}
